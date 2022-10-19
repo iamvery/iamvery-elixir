fb6e8a96baa6217c3d61d504c10f2f52ec6c9122
Jay Hayes <ur@iamvery.com>
2022-10-19 09:15:31 -0500

    Loosen elixir dependency

--- mix.exs
+++ mix.exs
@@ -17,8 +17,8 @@ defmodule Iamvery.MixProject do
   def project do
     [
       app: :iamvery,
-      version: "0.1.0",
-      elixir: "~> 1.14",
+      version: "0.2.0",
+      elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       description: @description,
       package: @package,
