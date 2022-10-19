a5e52c88c9eb43c17c9ed6581d51daf9feb76584
Jay Hayes <ur@iamvery.com>
2022-10-19 08:56:34 -0500

    Add package info to mix config

--- mix.exs
+++ mix.exs
@@ -1,12 +1,24 @@
 defmodule Iamvery.MixProject do
   use Mix.Project
 
+  @description """
+  """
+
+  @package [
+    files: ["lib", "mix.exs", "README"],
+    maintainers: ["Jay Hayes"],
+    licenses: ["MIT"],
+    links: %{"GitHub" => "https://github.com/iamvery/iamvery-elixir"}
+  ]
+
   def project do
     [
       app: :iamvery,
       version: "0.1.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
+      description: @description,
+      package: @package,
       deps: deps()
     ]
   end
