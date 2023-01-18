aac322e8200790c609e40165faf85232c7c6622f
Jay Hayes <ur@iamvery.com>
2023-01-18 11:28:28 -0600

    Fix envs for package publishing

--- mix.exs
+++ mix.exs
@@ -36,7 +36,7 @@ defmodule Iamvery.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ecto, "~> 3.9", only: :test},
+      {:ecto, "~> 3.9", only: [:dev, :test]},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
